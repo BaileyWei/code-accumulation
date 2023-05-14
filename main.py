10a17bc63cc8194cc77e6f385d6c8fdf9847eb9a
weiy1 <weiy1@LEGAL.REGN.NET>
2023-05-14 11:11:16 +0800

    code update full version

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from pinpoint_normalization.src.processor import PostProcessor
+from codeupdate_postprocess.src.processor import PostProcessor
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     import json
     import random
 
-    minibatch_gpt_path = r'./pinpoint_normalization/data/labeleddata_minibatch_for_gpt.json'
+    minibatch_gpt_path = r'./codeupdate_postprocess/data/labeleddata_minibatch_for_gpt.json'
     minibatch_gpt = json.load(open(minibatch_gpt_path, encoding="utf-8"))
     idx = random.sample(range(len(minibatch_gpt)), 1)[0]
     # id = 62YY-RKM1-F528-G323-00000-00_8_0
